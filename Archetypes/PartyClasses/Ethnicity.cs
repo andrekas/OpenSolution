42ea9034a7ca0087d098619c4688e869a31469d2
andrekas <alaurimaa@gmail.com>
2017-03-23 09:21:06 +0200

    TestClasses

--- Archetypes/PartyClasses/Ethnicity.cs
+++ Archetypes/PartyClasses/Ethnicity.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Open.Aids;
+﻿using Open.Aids;
 using Open.Archetypes.BaseClasses;
 
 
--- Tests/Archetypes/PartyClasses/EthnicityTests.cs
+++ Tests/Archetypes/PartyClasses/EthnicityTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Open.Tests.Archetypes.PartyClasses
+﻿namespace Open.Tests.Archetypes.PartyClasses
 {
     class EthnicityTests
     {

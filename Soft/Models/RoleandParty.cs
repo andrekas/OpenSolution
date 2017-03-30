19b48ab9de2c216d703d107d633c5c04d7d26c88
andrekas <alaurimaa@gmail.com>
2017-03-30 09:49:25 +0300

    MVC+

--- Soft/Models/RoleandParty.cs
+++ Soft/Models/RoleandParty.cs
@@ -3,9 +3,13 @@
     public class RoleandParty
     {
         public int ID { get; set; }
+        public string AllInOne { get { return GivenName + " " + LastName; }}
         public string GivenName { get; set; }
         public string LastName { get; set; }
         public Gender Gender { get; set; }
+       
+        public string Address { get; set; }
+
     }
 
     public enum Gender

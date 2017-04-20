7556b9ed33a468ba9053fb6f5fbf333e075e0044
Angelina <angelina.polschikova@gmail.com>
2017-04-20 13:12:52 +0300

    Locale + ISOCountryCode

---
+++ Archetypes/PartyClasses/ISOCountryCode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Open.Archetypes.PartyClasses
+{
+    public class ISOCountryCode: Locale
+
+    {
+        public static List<RegionInfo> GetCountriesByIso3166()
+        {
+            List<RegionInfo> countries = new List<RegionInfo>();
+            foreach (CultureInfo culture in CultureInfo.GetCultures(CultureTypes.SpecificCultures))
+            {
+                RegionInfo country = new RegionInfo(culture.LCID);
+                if (countries.Where(p => p.Name == country.Name).Count() == 0)
+                    countries.Add(country);
+            }
+            return countries.OrderBy(p => p.EnglishName).ToList();
+        }
+
+        public static List<RegionInfo> GetCountriesByCode(List<string> codes)
+        {
+            List<RegionInfo> countries = new List<RegionInfo>();
+            if (codes != null && codes.Count > 0)
+            {
+                foreach (string code in codes)
+                {
+                    try
+                    {
+                        countries.Add(new RegionInfo(code));
+                    }
+                    catch (Exception)
+                    {
+                        return null; //  Ignores the invalid culture code?
+                    }
+                }
+            }
+            return countries.OrderBy(p => p.EnglishName).ToList();
+        }
+    }
+}
